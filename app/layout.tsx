6424bce8312c5c1192396e849ec4d6083ccd3469
Kriti <kritikukreja6@gmail.com>
2026-09-08 23:34:02 +0530

    profile logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 // NEW: Import the Toaster
 import { Toaster } from "react-hot-toast";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Navbar />
         {children}
         {/* NEW: Add the Toaster component */}
         <Toaster 
